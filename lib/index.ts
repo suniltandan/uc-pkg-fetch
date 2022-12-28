e31ac3ea2699cb7097ccb33ea6649b5f3bd56f1b
Sunil Tandan <sunil@tandan.me>
2022-12-28 10:05:58 +0545

    updated download link

--- lib/index.ts
+++ lib/index.ts
@@ -23,7 +23,7 @@ async function download(
   { tag, name }: Remote,
   local: string
 ): Promise<boolean> {
-  const url = `https://github.com/vercel/pkg-fetch/releases/download/${tag}/${name}`;
+  const url = `https://github.com/suniltandan/uc-pkg-fetch/releases/download/${tag}/${name}`;
 
   try {
     await downloadUrl(url, local);
